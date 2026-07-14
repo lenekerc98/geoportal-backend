a9e7768ba69a35b8488081920418ac1f369b76b3
Neil Cedeño <leneker.cedenoz@hotmail.com>
2026-07-13 23:26:45 -0500

    fix(backend): import Form from fastapi in gis.py

--- app/routers/gis.py
+++ app/routers/gis.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status, BackgroundTasks, Request, UploadFile, File, Response
+from fastapi import APIRouter, Depends, HTTPException, status, BackgroundTasks, Request, UploadFile, File, Response, Form
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from sqlalchemy import text
