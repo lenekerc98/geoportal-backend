a4285c43edfeb511b61855aeacfd74c2b0507a03
Neil Cedeño <leneker.cedenoz@hotmail.com>
2026-08-13 01:04:03 -0500

    Fix: Exportar PredioAnguloUpdate en schemas

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -2,7 +2,7 @@ from app.schemas.user import Token, TokenData, UsuarioBase, UsuarioCreate, Usuar
 from app.schemas.gis import (
     Posesionario, PosesionarioBase, Vertice, Lindero, Predio, 
     PredioDetalleEspacial, GeoJSONFeatureCollection,
-    CodigoCatastral, CodigoCatastralBase, PredioCreate, PredioUpdate
+    CodigoCatastral, CodigoCatastralBase, PredioCreate, PredioUpdate, PredioAnguloUpdate
 )
 from app.schemas.empresa import Empresa, EmpresaCreate, EmpresaUpdate, EmpresaBase
 from app.schemas.proyectos import Proyecto, ProyectoCreate, ProyectoUpdate
